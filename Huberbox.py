b4502e1376d92f1c1b59ff6b1889aeac71dd3365
Laurenz Fedotoff <laurenz.fedotoff@gmail.com>
2022-05-12 18:16:43 +0200

    three bees in the process of adding

--- Huberbox.py
+++ Huberbox.py
@@ -30,6 +30,29 @@ def dfProcessingSingle(filename):
 
     return df
 
+def dfProcessingThree(filename):
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    df = pd.read_csv(os.path.join(path, 'data', 'ThreeBees', filename))
+    
+    df_new = df.drop(df.index[0])
+    df_new.loc[len(df_new)+1] = 0
+    df_new.reset_index(inplace=True)
+
+    # df['bee1_r+1'] = df_new['bee1_r']
+    # df['bee1_p+1'] = df_new['bee1_p']
+
+    # df['bee1_r+1'] = df['bee1_r+1']/22.35
+    # df['bee1_r'] = df['bee1_r']/22.35
+
+    # df['distancePerFrame'] = ((2 * math.pi * df['bee1_r'] * (df['bee1_p']/360)) + (2 * math.pi * df['bee1_r+1'] * (df['bee1_p+1']/360)))/2
+
+    # df['distancePerSec'] = df['distancePerFrame']/0.2
+
+    # df['distancePerSec'] = df['distancePerSec'].apply(lambda x: 0 if x <= 0.35 else x)
+    # df = df[:-1]
+
+    return df
 
 def main():
     path = os.path.dirname(os.path.realpath(__file__))
@@ -44,6 +67,7 @@ def main():
         elif 'deg36' in filename:
             df = dfProcessingSingle(filename)
             list_36.append(df)
+    
     
     plot_speed_single(list_26[0]['second'], list_26, 'plot_26.png')
     plot_speed_single(list_36[0]['second'], list_36, 'plot_36.png')
