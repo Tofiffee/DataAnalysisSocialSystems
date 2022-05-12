503100ffff810ec5e364297af4fae438fb8ce549
Laurenz Fedotoff <laurenz.fedotoff@gmail.com>
2022-05-12 08:46:55 +0200

    dkdkdk

--- DataProcessing.py
+++ DataProcessing.py
@@ -32,12 +32,24 @@ def DataPrepariation(df):
 
     return [x_axis, y_casu_1, y_casu_2]
 
+def CalcMean(df):
+    df_erst = df[['erst_A1', 'erst_A2',	'erst_A3', 'erst_A4', 'erst_A5', 'erst_A6', 'erst_A7', 'erst_B1', 'erst_B2', 'erst_B3', 'erst_B4', 'erst_B5', 'erst_B6', 'erst_B7',	'erst_C1', 'erst_C2', 'erst_C3', 'erst_C4']]
+    df_danach = df[['danach_A1', 'danach_A2',	'danach_A3', 'danach_A4', 'danach_A5', 'danach_A6', 'danach_A7', 'danach_B1', 'danach_B2', 'danach_B3', 'danach_B4', 'danach_B5', 'danach_B6', 'danach_B7',	'danach_C1', 'danach_C2', 'danach_C3', 'danach_C4']]
+
+    return df_erst, df_danach
+
+
 
 def main():
     df = ReadData()
     data_list = DataPrepariation(df)
     plotting_plot(data_list[0], data_list[1], data_list[2])
-    print(df.index.tolist())
+    df1, df2 = CalcMean(df)
+    df1['mean'] = df1.mean(axis=1)
+    df2['mean'] = df2.mean(axis=1)
+    print(df1.head(10))
+    print(df2.head(10))
+
 
 
 if __name__=='__main__':
