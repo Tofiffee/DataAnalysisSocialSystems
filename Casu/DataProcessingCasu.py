533dda637e5919de2fb53a3353ffb50637f59309
Laurenz Fedotoff <laurenz.fedotoff@gmail.com>
2022-06-09 19:41:56 +0200

    changing the graphs

--- Casu/DataProcessingCasu.py
+++ Casu/DataProcessingCasu.py
@@ -1,11 +1,11 @@
+from plotting_Casu import plotting_plot
+from DataProcessingCasu2 import plotBees, plotTemp, plotNoTempCont, plotTempCont
 import os
 
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-
-from plottingCasu2 import BoxplotCasu2
 
 def ReadData(url: str) -> pd.DataFrame: 
     """    This function downloads the data from a GoogleTable sheet with the given link and transformes them into a 
@@ -25,31 +25,34 @@ def ReadData(url: str) -> pd.DataFrame:
 
     return df
 
+def DataPrepariation(df):
+    casu = df.columns.values.tolist()
+    casu_1 = [x for x in casu if 'erst' in x]
+    casu_2 = [x for x in casu if 'danach' in x]
+
+
+    x_axis = df[f'{casu[0]}']
+    y_casu_1 = []
+    y_casu_2 = []
+    
+    for i in casu_1:
+        y_casu_1.append(df[f'{i}'])
+
+    for j in casu_2:
+        y_casu_2.append(df[f'{j}'])
+
+    return [x_axis, y_casu_1, y_casu_2]
+
 def CalcMean(df, list1, list2, BeeNumber):
     df1 = df[list1]
     df2 = df[list2]
     
     df1['mean'] = df1.mean(axis=1)
-    df2['mean'] = df1.mean(axis=1)
-    df1['mean'] = df1['mean']/BeeNumber * 100
-    df2['mean'] = df2['mean']/BeeNumber * 100
+    df2['mean'] = df2.mean(axis=1)
+    df1['mean'] = df1['mean'].apply(lambda x: x/BeeNumber * 100 if x != 0 else x)
+    df2['mean'] = df2['mean'].apply(lambda x: x/BeeNumber * 100 if x != 0 else x)
   
     return df1['mean'], df2['mean']
-
-# def dfProcessing(df, beeNumber, keyword1, keyword2):
-#     array1 = np.array([])
-#     array2 = np.array([])
-
-#     for column in df.columns.values.tolist()[1:]:
-#         df[f'{column}'] = df[f'{column}'].apply(lambda x: x/beeNumber*100)
-#         if keyword1 in column:
-#             array = df[f'{column}'].to_numpy()
-#             array1 = np.hstack((array1, array))
-#         elif keyword2 in column:
-#             array = df[f'{column}'].to_numpy()
-#             array2 = np.hstack((array2, array))
-
-#     return array1, array2
 
 def TestNormalDistribution(DataList):
     statistic1, p_shapiro1 = stats.shapiro(DataList[0])
@@ -90,44 +93,42 @@ def statisticalTestingIndependent(DataList):
         statistic, p_val = stats.mannwhitneyu(DataList[0], DataList[1], use_continuity=True, alternative='two-sided')
         return f'The MannWhitney-U test returns a p value of {p_val} with a statistic of {statistic}'
 
-df = ReadData('https://docs.google.com/spreadsheets/d/1c5eXRyP4uLSDKJ_1PHlx48nZ8wQ06mUr1Ayrty-kfDg/edit#gid=0')
-df_controlTemp = ReadData('https://docs.google.com/spreadsheets/d/1rtl7NfzKXGVWi90JLlAunum5BX6TK17NUiaficQs9q0/edit#gid=0')
 
-plotBees, plotTemp = CalcMean(
+def main():
+    df = ReadData('https://docs.google.com/spreadsheets/d/11DkhIY7QQkUMNtjX1b16j7DmmQ6woKyCrGO_3p-Avig/edit#gid=0')
+    df_Temp = ReadData('https://docs.google.com/spreadsheets/d/1c5eXRyP4uLSDKJ_1PHlx48nZ8wQ06mUr1Ayrty-kfDg/edit#gid=0')
+    df_controlTemp = ReadData('https://docs.google.com/spreadsheets/d/1rtl7NfzKXGVWi90JLlAunum5BX6TK17NUiaficQs9q0/edit#gid=0')
+    
+    data_list = DataPrepariation(df)
+    mean_casu1, mean_casu2 = CalcMean(
     df,
-    ['Bienen_A1', 'Bienen_A2', 'Bienen_B1', 'Bienen_B2', 'Bienen_D1', 'Bienen_D2', 'Bienen_D3','Bienen_D4', 'Bienen_E1', 'Bienen_F1', 'Bienen_F2', 'Bienen_F3', 'Bienen_F4'],
-    ['Temp_A1', 'Temp_A2', 'Temp_B1', 'Temp_B2', 'Temp_D1', 'Temp_D2', 'Temp_D3', 'Temp_D4', 'Temp_E1', 'Temp_F1', 'Temp_F2', 'Temp_F3', 'Temp_F4'],
-    6
-)
-plotNoTempCont, plotTempCont = CalcMean(
-    df_controlTemp,
-    ['CasunT_A', 'CasunT_B', 'CasunT_C', 'CasunT_D', 'CasunT_E', 'CasunT_F', 'CasunT_G', 'CasunT_H'],
-    ['CasuT_Alinks','CasuT_B_unten', 'CasuT_C_links', 'CasuT_D_unten', 'CasuT_E_rechts', 'CasuT_F_unten', 'CasuT_G_unten', 'CasuT_H_unten'],
-    5
-)
+    ['erst_A1', 'erst_A2',	'erst_A3', 'erst_A4', 'erst_A5', 'erst_A6', 'erst_A7', 'erst_B1', 'erst_B2', 'erst_B3', 'erst_B4', 'erst_B5', 'erst_B6', 'erst_B7',	'erst_C1', 'erst_C2', 'erst_C3', 'erst_C4'],
+    ['danach_A1', 'danach_A2',	'danach_A3', 'danach_A4', 'danach_A5', 'danach_A6', 'danach_A7', 'danach_B1', 'danach_B2', 'danach_B3', 'danach_B4', 'danach_B5', 'danach_B6', 'danach_B7',	'danach_C1', 'danach_C2', 'danach_C3', 'danach_C4'],
+    100
+    )
+
+    plotBees, plotTemp = CalcMean(
+        df_Temp,
+        ['Bienen_A1', 'Bienen_A2', 'Bienen_B1', 'Bienen_B2', 'Bienen_D1', 'Bienen_D2', 'Bienen_D3','Bienen_D4', 'Bienen_E1', 'Bienen_F1', 'Bienen_F2', 'Bienen_F3', 'Bienen_F4'],
+        ['Temp_A1', 'Temp_A2', 'Temp_B1', 'Temp_B2', 'Temp_D1', 'Temp_D2', 'Temp_D3', 'Temp_D4', 'Temp_E1', 'Temp_F1', 'Temp_F2', 'Temp_F3', 'Temp_F4'],
+        6
+    )
+    plotNoTempCont, plotTempCont = CalcMean(
+        df_controlTemp,
+        ['CasunT_A', 'CasunT_B', 'CasunT_C', 'CasunT_D', 'CasunT_E', 'CasunT_F', 'CasunT_G', 'CasunT_H'],
+        ['CasuT_Alinks','CasuT_B_unten', 'CasuT_C_links', 'CasuT_D_unten', 'CasuT_E_rechts', 'CasuT_F_unten', 'CasuT_G_unten', 'CasuT_H_unten'],
+        5
+        )
+
+    print(plotNoTempCont)
+
+    plotting_plot(
+        data_list[0], data_list[1], data_list[2], mean_casu1, mean_casu2,
+        plotBees, plotTemp, plotNoTempCont, plotTempCont
+        )
 
 
+if __name__=='__main__':
+    main()
 
-# TestDataDep = [
-#     [Temp_cont, noTemp_cont],
-#     [Temp, Bees]
-# ]
 
-# for i in TestDataDep:
-#     i_modified = TestNormalDistribution(i)
-#     result = statisticalTestingDependent(i_modified)
-#     print(result)
-
-# TestDataInd = [
-#     [noTemp_cont, Bees],
-#     [Temp, Temp_cont]
-#     ]
-
-# for i in TestDataInd:
-#     i_modified = TestNormalDistribution(i)
-#     result = statisticalTestingIndependent(i_modified)
-#     print(result)
-
-# labels = ['control\nN=7', 'empty vs Temp\nN=8', 'Bees vs Temp\nN=13']
-# path = os.path.realpath(os.path.dirname(__file__))
-# path = os.path.join(path, '..', 'graphs')
--- Casu/plotting_Casu.py
+++ Casu/plotting_Casu.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def plotting_plot(
     x_axis, y_casu_1, y_casu_2, mean_casu_1, mean_casu_2,
-    control1, control2, array1, array2, labels
+    plotBees, plotTemp, plotNoTempCont, plotTempCont
 ):
     path = os.path.dirname(os.path.realpath(__file__))
     
@@ -19,13 +19,14 @@ def plotting_plot(
     WIDTH = 0.25
 
     fig1 = plt.figure(figsize=(20, 15))
-    ax = plt.subplot(2, 10, (2, 10))
-    ax0 = plt.subplot(2, 10, (12, 17))
+    ax = plt.subplot(2, 10, (1, 10))
+    ax0 = plt.subplot(2, 10, (11, 16))
+    ax1 = plt.subplot(2, 10, (17, 20))
     ax.set_xlim(10, 960)
     ax.set_ylim(0, 10)
     ax.xaxis.set_major_locator(MultipleLocator(60))
-    ax.set_ylabel('Number of bees', fontsize=20, labelpad=10)
-    ax.set_xlabel('Time [s]', fontsize=20, labelpad=10)
+    ax.set_ylabel('Number of bees', fontsize=25, labelpad=10)
+    ax.set_xlabel('Time [s]', fontsize=25, labelpad=10)
     ax.tick_params(axis='both', which='major', labelsize=16)
     ax0.tick_params(axis='y', which='major', labelsize=16)
     ax.text(-35, 8.5, 'A', fontdict={'fontsize': 45})
@@ -37,62 +38,36 @@ def plotting_plot(
     for j in y_casu_2:
         ax.plot(x_axis, j, color='blue', alpha=0.5, linewidth=0.5, linestyle=(0, (5, 5)))
 
-    ax.plot(x_axis, mean_casu_1, color='red', linewidth=1)
-    ax.plot(x_axis, mean_casu_2, color='blue', linewidth=1)
+    ax.plot(x_axis, mean_casu_2, color='red', linewidth=1)
+    ax.plot(x_axis, mean_casu_1, color='blue', linewidth=1)
 
     ax.vlines(300, 0, 11, colors='k', linestyles='dashed')
     ax.vlines(320, 0, 11, colors='k', linestyles='dashed')
 
-    x_axis_boxplot = np.arange(len(labels))
+
     ax0.set_ylim(0, 100)
 
-    plota1 = ax0.boxplot(array1, positions=x_axis_boxplot - WIDTH/2 - 0.01, widths=WIDTH, notch=False, patch_artist=True,
-                boxprops=dict(facecolor='lightsteelblue', color='k'),
-                capprops=dict(color='k'),
-                whiskerprops=dict(color='k'),
-                flierprops=dict(color='k', markeredgecolor='k'),
-                medianprops=dict(color='red'))
-
-    plota2 = ax0.boxplot(array2, positions=x_axis_boxplot + WIDTH/2 + 0.01, widths=WIDTH, notch=False, patch_artist=True,
-                boxprops=dict(facecolor='lightcoral', color='k'),
-                capprops=dict(color='k'),
-                whiskerprops=dict(color='k'),
-                flierprops=dict(color='k', markeredgecolor='k'),
-                medianprops=dict(color='red'))
-
-    plotb1 = ax0.boxplot(control1, positions=x_axis_boxplot - WIDTH/2 - 0.01, widths=WIDTH, notch=False, patch_artist=True,
-                boxprops=dict(facecolor='dimgray', color='k'),
-                capprops=dict(color='k'),
-                whiskerprops=dict(color='k'),
-                flierprops=dict(color='k', markeredgecolor='k'),
-                medianprops=dict(color='red'))
-
-    plotb2 = ax0.boxplot(control2, positions=x_axis_boxplot + WIDTH/2 + 0.01, widths=WIDTH, notch=False, patch_artist=True,
-                boxprops=dict(facecolor='lightgray', color='k'),
-                capprops=dict(color='k'),
-                whiskerprops=dict(color='k'),
-                flierprops=dict(color='k', markeredgecolor='k'),
-                medianprops=dict(color='red'))
+    ax0.plot(x_axis, plotBees, color='blue', linewidth=1)
+    ax0.plot(x_axis, plotTemp, color='red', linewidth=1)
+    ax0.plot(x_axis, plotNoTempCont, color='blue', linewidth=1)
+    ax0.plot(x_axis, plotTempCont, color='red', linewidth=1)
 
 
-    ax0.grid(which='major', axis='y', linewidth=1, linestyle=(0, (1, 10)), color='black')
-    ax0.set_xticks(ticks=x_axis_boxplot, labels=labels, fontsize=16)
-    ax0.legend([plotb1['boxes'][0], plotb2['boxes'][0], plota1['boxes'][0], plota2['boxes'][0]], ['left/upper', 'right/lower' , '26 degree', '36 degree'], bbox_to_anchor=(1.2, 1.01), 
-    loc='upper right', fontsize=16)
+    ax0.legend(loc='upper right', fontsize=16)
     ax0.set_ylabel('Bees [%]', labelpad=10, fontsize=25)
-    ax0.set_xlabel('Experiments', labelpad=10, fontsize=25)
+    ax0.set_xlabel('Time [s]', labelpad=10, fontsize=25)
 
-    ax0.annotate('***', xy=(0, 85), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
-    ax0.annotate('***', xy=(1, 70), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
-    ax0.annotate('***', xy=(2, 60), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
+    # ax0.annotate('***', xy=(0, 85), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
+    # ax0.annotate('***', xy=(1, 70), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
+    # ax0.annotate('***', xy=(2, 60), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
 
-    x1, x2, y, h, col = 0.88, 1.86, 43, 2, 'k'
-    ax0.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=2, c=col)
-    ax0.text((x1+x2)*.5, y+h, "***", ha='center', va='bottom', color=col, fontsize=30)
+    # x1, x2, y, h, col = 0.88, 1.86, 43, 2, 'k'
+    # ax0.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=2, c=col)
+    # ax0.text((x1+x2)*.5, y+h, "***", ha='center', va='bottom', color=col, fontsize=30)
 
-    x1, x2, y, h, col = 1.13, 2.126, 58, -2, 'k'
-    ax0.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=2, c=col)
-    ax0.text((x1+x2)*.5, y+h, "***", ha='center', va='bottom', color=col, fontsize=30)
+    # x1, x2, y, h, col = 1.13, 2.126, 58, -2, 'k'
+    # ax0.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=2, c=col)
+    # ax0.text((x1+x2)*.5, y+h, "***", ha='center', va='bottom', color=col, fontsize=30)
 
     fig1.tight_layout()
     plt.savefig(os.path.join(path, '..', 'graphs', 'casuexp_1_line_plot.png'))
