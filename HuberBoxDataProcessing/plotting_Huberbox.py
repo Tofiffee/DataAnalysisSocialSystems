239f6c72ecc325c817fc3f05ea5b97e7f28d6812
Laurenz Fedotoff <laurenz.fedotoff@gmail.com>
2022-06-20 19:10:15 +0200

    some minor changes in plotting

--- HuberBoxDataProcessing/plotting_Huberbox.py
+++ HuberBoxDataProcessing/plotting_Huberbox.py
@@ -37,7 +37,7 @@ def boxplot(
 
     fig0, (ax0, ax1) = plt.subplots(2, 1, figsize=(12, 10))
     AXIS = (ax0, ax1)
-    ax0.set_ylim(0, 80)
+    ax0.set_ylim(0, 40)
     ax1.set_ylim(0, 7)
     
     # all settings that are needed for both plots
@@ -79,33 +79,35 @@ def boxplot(
 
     ax0.set_xticks(ticks=x_axis_A, labels=label_A, fontsize=16)
     ax1.set_xticks(ticks=x_axis_B, labels=label_B, fontsize=16)
-    ax0.legend([plota1["boxes"][0], plota2["boxes"][0]], ['26 Degrees', '36 Degrees'], bbox_to_anchor=(1.005, 1), loc='upper left', fontsize=16)
-    ax1.legend([plotb1["boxes"][0], plotb2["boxes"][0]], ['26 Degrees', '36 Degrees'], bbox_to_anchor=(1.005, 1.01), loc='upper left', fontsize=16)
+    ax0.legend([plota1["boxes"][0], plota2["boxes"][0]], ['26 °C', '36 °C'], bbox_to_anchor=(1.005, 1), loc='upper left', fontsize=16)
+    ax1.legend([plotb1["boxes"][0], plotb2["boxes"][0]], ['26 °C', '36 °C'], bbox_to_anchor=(1.005, 1.01), loc='upper left', fontsize=16)
     ax0.set_ylabel('Social Contact duration [s]', fontsize=16)
     ax1.set_ylabel('Walking speed [cm/s]', fontsize=16)
+    ax0.set_xlabel('Number of Bees', labelpad=10, fontsize=16)
+    ax1.set_xlabel('Number of Bees', labelpad=10, fontsize=16)
     ax0.annotate('*', xy=(0, 15), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
     ax1.annotate('***', xy=(0, 5.5), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
     ax1.annotate('***', xy=(1, 5.5), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
     ax1.annotate('***', xy=(2, 5.5), xytext=(0, 0), textcoords='offset points', ha='center', va='bottom', fontsize=25)
-    ax0.text(-0.85, 21, 'A', fontdict={'fontsize': 45})
-    ax1.text(-0.9, 6.1, 'B', fontdict={'fontsize': 45})
+    ax0.text(-0.5, 35, 'A', fontdict={'fontsize': 45})
+    ax1.text(-0.5, 6.1, 'B', fontdict={'fontsize': 45})
     ax0.tick_params(axis='y', which='major', labelsize=16)
     ax1.tick_params(axis='y', which='major', labelsize=16)
     
-    ax0.text(-0.2, 11, 'n=11', fontdict={'fontsize': 16})
-    ax0.text(0.08, 22, 'n=11', fontdict={'fontsize': 16})
-    ax0.text(0.82, 16, 'n=6', fontdict={'fontsize': 16})
-    ax0.text(1.08, 18, 'n=6', fontdict={'fontsize': 16})
-    ax0.text(1.8, 6, 'n=6', fontdict={'fontsize': 16})
-    ax0.text(2.08, 5, 'n=6', fontdict={'fontsize': 16})
+    ax0.text(-0.23, 11, 'n=11', fontdict={'fontsize': 16})
+    ax0.text(0.06, 22, 'n=11', fontdict={'fontsize': 16})
+    ax0.text(0.77, 16, 'n=6', fontdict={'fontsize': 16})
+    ax0.text(1.06, 18, 'n=6', fontdict={'fontsize': 16})
+    ax0.text(1.77, 6, 'n=6', fontdict={'fontsize': 16})
+    ax0.text(2.06, 5, 'n=6', fontdict={'fontsize': 16})
 
-    ax1.text(-0.3, 2.4, 'n=6', fontdict={'fontsize': 16})
-    ax1.text(0.18, 3, 'n=5', fontdict={'fontsize': 16})
+    ax1.text(-0.35, 2.3, 'n=6', fontdict={'fontsize': 16})
+    ax1.text(0.15, 3, 'n=5', fontdict={'fontsize': 16})
     
-    ax1.text(0.65, 2.3, 'n=22', fontdict={'fontsize': 16})
+    ax1.text(0.6, 2.3, 'n=22', fontdict={'fontsize': 16})
     ax1.text(1.18, 2.8, 'n=22', fontdict={'fontsize': 16})
     
-    ax1.text(1.7, 2.1, 'n=6', fontdict={'fontsize': 16})
+    ax1.text(1.65, 2.1, 'n=6', fontdict={'fontsize': 16})
     ax1.text(2.18, 2.4, 'n=6', fontdict={'fontsize': 16})
 
 
